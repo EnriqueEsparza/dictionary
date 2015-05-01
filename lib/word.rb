e86cea8b99e4d7465db6dc38e42a523a7e610427
Enrique <iqueart@yahoo.com>
2015-05-01 11:42:31 -0700

    pass all rspec test for Word class

--- lib/word.rb
+++ lib/word.rb
@@ -1,4 +1,5 @@
 class Word
+  @@words = []
 
   define_method(:initialize) do |word|
     @word = word
@@ -7,6 +8,19 @@ class Word
 
   define_method(:word) do
     @word
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
   end
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,13 +2,40 @@ require ('rspec')
 require ('word')
 
 describe(Word) do
+  before() do
+    Word.clear()
+  end
 
-end
-
-describe("#new_word") do
+describe("#word") do
   it("adds a new word to the dictionary") do
     test_word = Word.new("apple")
     test_word.save()
-    expect(test_word.new_word()).to(eq("apple"))
+    expect(test_word.word()).to(eq("apple"))
   end
+end
+
+describe(".all") do
+  it("initially has no words on file") do
+    expect(Word.all()).to(eq([]))
+  end
+end
+
+
+
+describe("#save") do
+  it("adds a word to the array of words") do
+    test_word = Word.new("apple")
+    test_word.save()
+    expect(Word.all()).to(eq([test_word]))
+  end
+end
+
+describe(".clear") do
+  it("empties out all saved words") do
+    Word.new("apple").save()
+    Word.clear()
+    expect(Word.all()).to(eq([]))
+  end
+end
+
 end
